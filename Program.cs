7896d224ce0c795f93b2a41f04a86b58437207f0
PeemmegaTH <theeridth.yodk@kmutt.ac.th>
2023-04-03 10:53:55 +0700

    Sussy 2

--- Program.cs
+++ Program.cs
@@ -1,76 +1,75 @@
 ﻿class Program {
    
-  
-    static float InputText(float preVal, string mode){
-        Console.Write("Input number or End to finished: ");
-        string InputValue = (Console.ReadLine());
-
-        mode = "";
-        float output = 0.0f;
-        float number = 0;
-            if (InputValue != "End"){
-                number = float.Parse(InputValue);
-
-                float nextNumber = InputText(number,mode);
-                
-                Console.WriteLine("Mode is: " + mode);
-
-                    switch (mode){
-                    case "FindMax":
-                    Console.Write(preVal + " ," + number);
-                    
-                    if (number <= preVal){
-                        output = number;
-                    }
-                    break;
-                    case "FindMin":
-                    if (number >= nextNumber){
-                        output = number;
-                    }
-                    break;
-                    case "FindMean":
-                    output = number + preVal/2;
-                    break;
-                    
-                    }           
-                }
-            
-            
-            if (InputValue == "End" ){
-                Console.Write(preVal + " ,");
-
-                Console.WriteLine(number);
-                bool selected = false;
-                while (!selected){
-                    Console.WriteLine("Input mode [FindMax ,FindMin,FindMean] ");
-                    Console.Write("Press here: ");
-                    mode = (Console.ReadLine());
-
-                    switch (mode){
-                    case "FindMax":
-                    selected = true;
-                   
-                    break;
-                    case "FindMin":
-                    selected = true;
-                    
-                    break;
-                    case "FindMean":
-                    selected = true;
-                    break;
-                    default:
-                    Console.Write("Invalid mode");
-                    break;
-                    }            
-                }
-            }
-          
-
-        return output;
-    }
-
     static void Main(string[] args){
-        float output = InputText(0,"");
+        string modeselect = "";
+
+        static float InputText(float preVal,ref string mode){
+            Console.Write("Input number or End to finished: ");
+            string InputValue = (Console.ReadLine());
+
+            float output = 0.0f;
+            float number = 0;
+                if (InputValue != "End"){
+                    number = float.Parse(InputValue);
+
+
+                    float nextNumber = InputText(number,ref mode);
+                    
+                    //Console.WriteLine("Mode is: " + mode);
+
+                        switch (mode){
+                        case "FindMax":
+                        
+                        if (number >= preVal){
+                            output = number;
+                        }
+                        break;
+                        case "FindMin":
+                        if (number <= preVal){
+                            output = number;
+                        }
+                        break;
+                        case "FindMean":
+                        output = number + preVal/2;
+                        break;
+                        
+                        }           
+                    }
+                
+                
+                if (InputValue == "End" ){
+                    
+                    bool selected = false;
+                    while (!selected){
+                        Console.WriteLine("Input mode [FindMax ,FindMin,FindMean] ");
+                        Console.Write("Press here: ");
+                        mode = (Console.ReadLine());
+
+                        switch (mode){
+                        case "FindMax":
+                        selected = true;
+                    
+                        break;
+                        case "FindMin":
+                        selected = true;
+                        
+                        break;
+                        case "FindMean":
+                        selected = true;
+                        break;
+                        default:
+                        Console.Write("Invalid mode");
+                        break;
+                        }            
+                    }
+                }
+            
+            return output;
+        }
+        Console.Write("Input number or End to finished: ");
+        float preVal = float.Parse(Console.ReadLine());
+
+        float output = InputText(preVal,ref modeselect);
         Console.WriteLine(output);
     }
-}
+ }
